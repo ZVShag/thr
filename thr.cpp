c6951b3f9292a22824e9500db780893b3eb57fcb
MarshallMathers1313 <AshurovAbdulla13>
2022-05-23 17:43:58 +0500

    end

--- thr.cpp
+++ thr.cpp
@@ -1,11 +1,17 @@
 ﻿#include <iostream> 
 #include<thread>
-#include<mutex>
 using namespace std;
-mutex m1, m2;
 // Динамический массив совершенных чисел 
 // Динамический массив чисел фибоначи
 // длинyу массивов задает пользователь 
+int fl(int i)
+{
+    if (i > 1)
+        return i * fl(i - 1);/// n*(n-1)!
+    if (i >=0)
+        return 1;
+
+}
 class sov
 {
     int* a;
@@ -42,7 +48,7 @@ public:
         this_thread::sleep_for(chrono::microseconds(200));
         for (int i = 0; i < n; i++)
         {
-            cout << "thread ID: "<<this_thread::get_id() << " " << "Prost number " << a[i] << endl;
+            cout <<endl<< "thread ID: "<<this_thread::get_id() << " " << "Prost number " << a[i] << endl;
             this_thread::sleep_for(chrono::microseconds(500));
         }
     }
@@ -75,7 +81,7 @@ public:
         this_thread::sleep_for(chrono::microseconds(200));
         for (int i = 0; i < n; i++)
         {
-            cout <<"thread ID: "<<this_thread::get_id() << " " << "Fibonachi number  " << d[i] << endl;
+            cout <<endl<<"thread ID: "<<this_thread::get_id() << " " << "Fibonachi number  " << d[i] << endl;
             this_thread::sleep_for(chrono::microseconds(500));
         }
 
@@ -86,17 +92,48 @@ public:
     }
 
 };
+class factorial
+{
+    int* c;
+public:
+    void Factorial(int n)
+    {
+        this->c = new int[n];
+        for (int i = 0; i < n+1; i++)
+        {
+            c[i] = fl(i);
+            
+        }
+        this_thread::sleep_for(chrono::microseconds(200));
+        for (int i = 0; i < n; i++)
+        {
+            cout << endl << "thread ID: " << this_thread::get_id() << " " << "Factorial  " << c[i] << endl;
+            this_thread::sleep_for(chrono::microseconds(500));
+        }
+    }
+    ~factorial()
+    {
+        delete[] c;
+    }
+
+};
+/*
+5!=5*4! n!=n*(n-1)!
+4!=4*3!
+3!=3*2!
+
+*/
 int main()
 {
-    
     int n = 10;
     fib r;
+    factorial c;
     thread thread1(&fib::Fib, r, n);
     sov v;
-    thread thread2(&sov::Sov, &v, n);
-    //r.Print(n);
-    //v.Print(n);
+    thread thread2(&sov::Sov, v, n);
+    thread thread3(&factorial::Factorial, c, n);
     thread1.join();
     thread2.join();
+    thread3.join();
     return 0;
 }
